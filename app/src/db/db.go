69bb290413ea247d755edbf5ed1265f59bfb2614
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-25 11:07:55 +0900

    migrate user

--- app/src/db/db.go
+++ app/src/db/db.go
@@ -6,15 +6,15 @@ import (
 
 	"github.com/Shota-Kurahashi/ent-with-echo/ent"
 	"github.com/Shota-Kurahashi/ent-with-echo/src/config"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func Connect() *ent.Client {
-	client, err := ent.Open("mysql", config.GetMySQLUrl())
+	client, err := ent.Open("mysql", config.GetMySqlUrl())
 
 	if err != nil {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
-	defer client.Close()
 
 	return client
 }
